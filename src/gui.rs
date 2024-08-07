f3509a3da72f90240099c62c9888b301af15cafd
Kenneth Love <kennethlove@gmail.com>
2024-08-06 17:40:08 -0700

    All sorts of changes to try to make things work. Things aren't working.

--- src/gui.rs
+++ src/gui.rs
@@ -12,9 +12,9 @@ pub fn main() {
 }
 
 #[component]
-fn StreaksTable() -> Element {
-    let mut db = use_context::<Database>();
-    let streaks = db.get_all().unwrap();
+fn StreaksTable(mut db: Signal<Database>) -> Element {
+    // let mut db = use_context::<Database>();
+    let streaks = db().get_all().unwrap();
     let streaks = streaks.into_values();
 
     rsx! {
@@ -31,7 +31,7 @@ fn StreaksTable() -> Element {
             }
             tbody {
                 for streak in streaks {
-                    StreakListing { streak }
+                    StreakListing { streak: streak, db: db }
                 }
             }
         }
@@ -48,83 +48,87 @@ fn check_in(streak_id: Uuid) -> Streak {
     streak
 }
 
-fn add_streak(streak: Streak) {
-    let mut db = use_context::<Database>();
-    db.add(streak).unwrap();
-    db.save().unwrap();
+fn add_streak(streak: Streak, mut db: Signal<Database>) {
+    db().add(streak).unwrap();
+    db().save().unwrap();
 }
 
 #[component]
-fn StreakListing(streak: Streak) -> Element {
-    let mut db = use_context::<Database>();
-    let mut streak = use_signal(|| streak);
+fn CheckInButton(streak_id: Uuid, db: Signal<Database>) -> Element {
+    // let mut db = use_context::<Database>();
+    rsx! {
+        button {
+            class: "button is-primary is-small",
+            onclick: move |_| {
+                let updated_streak = check_in(streak_id);
+                db().update(streak_id, updated_streak).unwrap();
+            },
+            "CHECK IN"
+        }
+    }
+}
 
-    let date = match streak().last_checkin {
+#[component]
+fn RemoveButton(streak_id: Uuid, db: Signal<Database>) -> Element {
+    rsx! {
+        button {
+            class: "button is-danger is-small",
+            onclick: move |_| {
+                let confirm = MessageDialog::new()
+                    .set_type(MessageType::Info)
+                    .set_title("Remove Streak")
+                    .set_text("Are you sure you want to remove this streak?")
+                    .show_confirm()
+                    .unwrap();
+
+                match confirm {
+                    true => {
+                        db().delete(streak_id).unwrap();
+                        db().save().unwrap();
+                    }
+                    _ => {}
+                }
+            },
+            "REMOVE"
+        }
+    }
+}
+
+#[component]
+fn StreakListing(streak: Streak, db: Signal<Database>) -> Element {
+    let date = match streak.last_checkin {
         Some(date) => date.format("%Y-%m-%d").to_string(),
         None => "Never".to_string(),
     };
-    let emoji = streak().emoji_status();
+    let emoji = streak.emoji_status();
 
     rsx! {
         tr {
-            td { "{streak().task}" }
-            td { "{streak().frequency}" }
+            td { "{streak.task}" }
+            td { "{streak.frequency}" }
             td { "{emoji}" }
             td { "{date}" }
-            td { "{streak().total_checkins}" }
+            td { "{streak.total_checkins}" }
             td {
-                button {
-                    class: "button is-primary is-small",
-                    onclick: move |_| {
-                        let updated_streak = check_in(streak().id);
-                        streak.set(updated_streak);
-                    },
-                    "CHECK IN"
-                }
+                CheckInButton { streak_id: streak.id, db: db }
             }
             td {
-                button {
-                    class: "button is-danger is-small",
-                    onclick: move |_| {
-                        let yes = MessageDialog::new()
-                            .set_type(MessageType::Info)
-                            .set_title("Delete Streak")
-                            .set_text(&format!("Are you sure you want to delete {}?", streak().task))
-                            .show_confirm()
-                            .unwrap();
-                        if yes {
-                            match db.delete(streak().id) {
-                                Ok(_) => {
-                                    db.save().unwrap();
-                                }
-                                Err(e) => {
-                                    MessageDialog::new()
-                                        .set_type(MessageType::Error)
-                                        .set_title("Error")
-                                        .set_text(&format!("Error deleting streak: {}", e))
-                                        .show_alert()
-                                        .unwrap();
-                                }
-                            }
-                        }
-                    },
-                    "REMOVE"
-                }
+                RemoveButton { streak_id: streak.id, db: db }
             }
         }
     }
 }
 
 #[component]
-fn Streaks() -> Element {
+fn Streaks(db: Signal<Database>) -> Element {
     rsx! {
-        div { class: "panel-block", StreaksTable {} }
-        div { class: "panel-block", NewStreak {} }
+        div { class: "panel-block", StreaksTable { db } }
+        div { class: "panel-block", NewStreak { db } }
     }
 }
 
 fn App() -> Element {
-    use_context_provider(|| Database::new(&get_database_url()).unwrap());
+    let mut db = use_signal(|| Database::new(&get_database_url()).unwrap());
 
     rsx! {
         link { rel: "stylesheet", href: "assets/bulma.min.css" }
@@ -140,15 +144,14 @@ fn App() -> Element {
                         }
                     }
                 }
-                Streaks {}
+                Streaks { db }
             }
         }
     }
 }
 
 #[component]
-fn NewStreak() -> Element {
-    let mut db = use_context::<Database>();
+fn NewStreak(db: Signal<Database>) -> Element {
     let mut new_streak = use_signal(|| "".to_string());
     let mut new_streak_type = use_signal(|| "Daily".to_string());
 
@@ -181,7 +184,7 @@ fn NewStreak() -> Element {
                     }
                     _ => Streak::default(),
                 };
-                add_streak(new);
+                add_streak(new, db);
             },
             "Add New"
         }
